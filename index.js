257965a1aee5ddf51aa8e45e7e083f122e9bee2b
Triwith <triwith.m@ku.th>
2017-08-25 18:21:51 +0700

    Up File

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "500px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function main(){
+
+}
+$().ready(main());
