4d9dae764548624fae3db1d0945c6cad26f9c4ca
famefryer12 <triwith.m@ku.th>
2018-02-16 23:41:10 +0700

    Add detail

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "30vw";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
 function main(){
 
 }
